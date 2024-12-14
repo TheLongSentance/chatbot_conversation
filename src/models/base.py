f12716fe009f5dd888b241b76185d32a416e4bae
Brian Sentance <35327158+TheLongSentance@users.noreply.github.com>
2024-12-14 10:40:29 +0000

    Added template system prompt to prefix user-supplied system prompt

--- src/models/base.py
+++ src/models/base.py
@@ -28,9 +28,15 @@ class ChatbotBase(ABC, Generic[T]):
     
     _total_count: int = 0  # Class variable to track total instances
 
+    SYSTEM_PROMPT_TEMPLATE: str = "You are about to take part in a conversation with multiple participants. " \
+    "Your identity in this conversation is {name} to enable you to refer to yourself. Other participants " \
+    "will also have names that you can use to refer to them if needed, often in the example form of \"name: \" at the " \
+    "start of their contributions to the conversation. Follow the role instructions supplied in this prompt. {system_prompt}"
+
     def __init__(self, model_version: str, system_prompt: str, name: str):
         self.model_version: str = model_version
-        self.system_prompt: str = system_prompt
+        self.system_prompt: str = self.SYSTEM_PROMPT_TEMPLATE.format(
+            name=name, system_prompt=system_prompt)
         self.name: str = name
         self.api = self._initialize_api()
         ChatbotBase._total_count += 1
