039731f8fe01d89e424c23217e17a98c0f6e6b30
Brian Sentance <35327158+TheLongSentance@users.noreply.github.com>
2025-01-09 13:53:18 +0000

    defined what needs to be exported from the package

--- src/chatbot_conversation/__init__.py
+++ src/chatbot_conversation/__init__.py
@@ -1,3 +1,29 @@
 """
 This package contains the modules for the chatbot conversation.
 """
+
+from chatbot_conversation.conversation import (
+    ConfigurationLoader,
+    ConversationConfig,
+    ConversationManager,
+)
+from chatbot_conversation.models import (
+    BotConfig,
+    ChatbotBase,
+    ChatbotFactory,
+    ConversationMessage,
+)
+from chatbot_conversation.utils import APIConfig
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "ConversationManager",
+    "ConfigurationLoader",
+    "ConversationConfig",
+    "ChatbotBase",
+    "BotConfig",
+    "ConversationMessage",
+    "ChatbotFactory",
+    "APIConfig",
+]
